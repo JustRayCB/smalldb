71a98dccd20cd4c69e5ec00350e5e866652cf86b
JustRayCB <rayancbpro@gmail.com>
2022-12-03 11:07:12 +0100

    Add query files

---
+++ query.hpp
@@ -0,0 +1,18 @@
+#ifndef _QUERY_HPP
+#define _QUERY_HPP
+
+#include <vector>
+#include <string>
+
+#include "db.hpp"
+
+
+std::vector<std::string> select(database_t *database, std::string query);
+std::vector<std::string> insert(database_t *database, std::string query);
+std::vector<std::string> deletion(database_t *database, std::string query);
+std::vector<std::string> update(database_t *database, std::string query);
+bool findStudent(database_t *database, std::string &field, std::string &value, std::vector<std::string> &results);
+
+
+#endif
+
