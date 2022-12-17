9b2ec4ac968446f4b47311accc74385ae029657c
JustRayCB <rayancbpro@gmail.com>
2022-12-17 19:22:44 +0100

    Last doctring

--- utils.hpp
+++ utils.hpp
@@ -12,6 +12,14 @@ struct Client{
 int check(int exp, const char *msg);
 
 /*
+ * @brief: Function that will send the results to the client
+ * @param client: ref to the client
+ * @param results: vector that contains the results
+ * @return 1 if it went well and 0 otherwise
+ */
+int printResult(const Client &client, std::vector<std::string> &results);
+
+/*
  * @brief: Main function for a thread
  *
  * @param client: Client struct with the client socket and a link to the database
